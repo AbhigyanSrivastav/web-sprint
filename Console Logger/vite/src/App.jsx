3eb81e4b4a26c52c798e032242f03382c4dc3737
rahulkumar <zeddx.rahul@gmail.com>
2025-03-20 14:14:20 +0530

    2nd commit

--- Console Logger/vite/src/App.jsx
+++ Console Logger/vite/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import ContactHome from './pages/Contact';
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<Home />}></Route>
+    <Route path="/contact" element={<ContactHome />}></Route>
     </Routes>
     </BrowserRouter>
   )
