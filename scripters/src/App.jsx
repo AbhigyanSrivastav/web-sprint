b7c028d9e6657b67a5b477c13ad5bbee632de705
aman sharma <amansharmayt1908@gmail.com>
2025-03-20 13:31:48 +0530

    second commit and push

--- scripters/src/App.jsx
+++ scripters/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import ProductsLand from './components/ProductsLand'
+import AllProducts from './components/AllProducts'
 const App = () => {
   return (
     <div>
       < NavBar />
       < ProductsLand />
+      < AllProducts />
     </div>
   )
 }
--- scripters/src/components/ProductsLand.jsx
+++ scripters/src/components/ProductsLand.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import './ProductsLand.css'
 const ProductsLand = () => {
   return (
     <div>
       <div className="product-land">
         <div className="imageLeft">
             <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT-fvGkV1G3aGuA6TwoY6UTVDuaFPBACVudjnfcR6ras9jTo2GZjtEKUsy2SmtrAy63vEukBkEWPA97so8pdR4d0SYgObwjBus12Ca4qrWN4a22kfQnELDO" alt="book" />
-            
+
+        </div>
+        <div className="image-detail">
+            <h2>Hadappa - Vineet Bajpaye</h2>
+            <p>Price: 2000rs</p>
+            <button>Add to Cart</button>
         </div>
       </div>
     </div>
